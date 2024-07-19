b09ea09939419825647d9ec9a950acd42a860f3c
Hritvik Gupta <hritvikgupta@brb-1002.apng.wlan.private.upenn.edu>
2024-07-19 14:41:13 -0400

    updated notion issue

--- update_notion_issues.py
+++ update_notion_issues.py
@@ -2,7 +2,7 @@ import os
 import requests
 from datetime import datetime
 
-# Environment variables
+# Environment variable
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 NOTION_TOKEN = os.getenv('NOTION_TOKEN')
 NOTION_ISSUES_DATABASE = os.getenv('NOTION_ISSUES_DATABASE')
